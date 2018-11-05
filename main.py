47899e3f19c092c4567bdc126e19d961e6b5354f
mohsinister <mohsin@mohsinister@gmail.com>
2018-11-05 15:04:17 +0500

    tmdb + omdbapi search

--- main.py
+++ main.py
@@ -4,6 +4,54 @@ import json
 import re
 
 apiKey = 'XXXX'
+finaljson = ''
+api_key= 'XXXX'
+def dataSetter(json1):
+    movieTitle = moviename
+    movieYear = ''
+    movieRuntime = ''
+    movieGenre = ''
+    moviePlot = ''
+    movieMeta = ''
+    movieImdb = ''
+    movieActors = ''
+    if json1['Runtime'] == "N/A":
+        movieRuntime = "0"
+    if json1['Year']:
+        movieYear = json1['Year']
+        movieYear= str(movieYear).replace('"','\\"')
+    if (json1['Runtime']):
+        movieRuntime = json1['Runtime']
+        movieRuntime= str(movieRuntime).replace('"','\\"')
+    if json1['Genre']:
+        movieGenre = json1['Genre']
+        movieGenre= str(movieGenre).replace('"','\\"')
+    if json1['Plot']:
+        moviePlot = json1['Plot']
+        moviePlot= str(moviePlot).replace('"','\\"')
+    if json1['Metascore'] == "N/A":
+        movieMeta = 0
+    elif json1['Metascore']:
+        movieMeta = json1['Metascore']
+        movieMeta= str(movieMeta).replace('"','\\"')
+    if json1['imdbRating'] == "N/A":
+        movieImdb = 0
+    elif json1['imdbRating']:
+        movieImdb = json1['imdbRating']
+        movieImdb= str(movieImdb).replace('"','\\"')
+    if json1['Actors']:
+        movieActors = json1['Actors']
+        movieActors= str(movieActors).replace('"','\\"')
+    print (originalfile + " was successful")
+    jsonpart = '\t\t\t["{}","{}","{}","{}","{}","{}","{}","{}","{}"]'.format(counter,movieTitle, movieYear, movieRuntime, movieImdb, movieMeta, moviePlot, movieGenre, movieActors)
+    if counter != totalmovies:
+        global finaljson
+        finaljson = ''.join([finaljson, jsonpart, " , \n"])
+                # put coma
+    else:
+        #global finaljson
+        finaljson = ''.join([finaljson, jsonpart])
+    return finaljson
 currentDir = os.getcwd()
 print ("Your current directory is : ")
 print (currentDir)
@@ -28,12 +76,9 @@ counter = 0
 errorflag = 0
 finaljson = "{\n\t\"data\": \n\t\t[\n"
 errorfile = open("ERRORLOG", "w")
-
 for filename in dirs:
     if filename != 'main.py' and filename!='content.txt' and filename!='index.html' and filename!='ERRORLOG':
-        #print ("ok")
         originalfile = filename
-        #print (filename)
         filename = filename.replace("(", "")
         filename = filename.replace(")", "")
         filename = filename.replace("[", "")
@@ -69,52 +114,31 @@ for filename in dirs:
         details = fetchedDetails.content
         json1 = json.loads(details)
         if json1['Response'] == "False":
-            errorflag = 1
-            errorfile.write(originalfile + " -- API Error : " + json1['Error'] + " URL : " + url + "\n")
+            url = 'https://api.themoviedb.org/3/search/movie?query={0}&api_key={1}'.format(moviename,api_key)
+            fetchedDetails = requests.get(url)
+            details = fetchedDetails.content
+            json2 = json.loads(details)
+            if json2['total_results'] > 0:
+                tmdb = json2['results'][0]['id']
+                url = 'https://api.themoviedb.org/3/movie/{0}?&api_key={1}'.format(tmdb,api_key)
+                fetchedDetails = requests.get(url)
+                details = fetchedDetails.content
+                json2 = json.loads(details)
+                imdb = json2['imdb_id']
+                if imdb != "":
+                    url = 'http://www.omdbapi.com/?apikey={0}&i={1}'.format(apiKey,imdb)
+                    fetchedDetails = requests.get(url)
+                    details = fetchedDetails.content
+                    json2 = json.loads(details)
+                    finaljson = dataSetter(json2)
+                else:
+                    errorflag = 1
+                    errorfile.write(originalfile + " -- API Error : " + json1['Error'] + " URL : " + url + "\n")
+            else: 
+                errorflag = 1
+                errorfile.write(originalfile + " -- API Error : " + json1['Error'] + " URL : " + url + "\n")
         else:
-            movieTitle = moviename
-            movieYear = ''
-            movieRuntime = ''
-            movieGenre = ''
-            moviePlot = ''
-            movieMeta = ''
-            movieImdb = ''
-            movieActors = ''
-            if json1['Runtime'] == "N/A":
-            	continue
-            if json1['Year']:
-                movieYear = json1['Year']
-                movieYear= str(movieYear).replace('"','\\"')
-            if (json1['Runtime']):
-                movieRuntime = json1['Runtime']
-                movieRuntime= str(movieRuntime).replace('"','\\"')
-            if json1['Genre']:
-                movieGenre = json1['Genre']
-                movieGenre= str(movieGenre).replace('"','\\"')
-            if json1['Plot']:
-                moviePlot = json1['Plot']
-                moviePlot= str(moviePlot).replace('"','\\"')
-            if json1['Metascore'] == "N/A":
-                movieMeta = 0
-            elif json1['Metascore']:
-                movieMeta = json1['Metascore']
-                movieMeta= str(movieMeta).replace('"','\\"')
-            if json1['imdbRating'] == "N/A":
-                movieImdb = 0
-            elif json1['imdbRating']:
-                movieImdb = json1['imdbRating']
-                movieImdb= str(movieImdb).replace('"','\\"')
-            if json1['Actors']:
-                movieActors = json1['Actors']
-                movieActors= str(movieActors).replace('"','\\"')
-            print (originalfile + " was successful")
-            jsonpart = '\t\t\t["{}","{}","{}","{}","{}","{}","{}","{}","{}"]'.format(counter,movieTitle, movieYear, movieRuntime, movieImdb, movieMeta, moviePlot, movieGenre, movieActors)
-            if counter != totalmovies:
-                finaljson = ''.join([finaljson, jsonpart, " , \n"])
-                # put coma
-            else:
-                finaljson = ''.join([finaljson, jsonpart])
-
+            finaljson = dataSetter(json1)
 finaljson = finaljson.rstrip(", \n")
 finaljson = ''.join([finaljson, "\n\t\t]\n}"])
 fh = open("content.txt", "w")
