7e6ed07db61da0904dfa66029765b5f18641c912
mohsinister <mohsin@mohsinister@gmail.com>
2018-11-12 14:30:55 +0500

    Read local file first to fetch data

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
 import os
 import requests
 import json
 import re
+from pathlib import Path
 
 omdbapiKey = 'XXXXX'
 tmdbKey = 'XXXXXXXXXXXXXX'
@@ -51,12 +53,19 @@ def dataSetter(json1):
         finaljson = ''.join([finaljson, jsonpart])
     return finaljson
 currentDir = os.getcwd()
+my_file = Path(currentDir + "/content.txt")
+oldjson = ""
+if my_file.is_file():
+    fr = open("content.txt", "r")
+    oldjson = fr.read()
+    fr.close()
 print ("Your current directory is : ")
 print (currentDir)
+
 flag = input("Is this where your movies are? (y/n): ")
 
 if(flag=="n"):
-	currentDir = input("Please input the full address to your directory where movies are.\nDir:\t")
+    currentDir = input("Please input the full address to your directory where movies are.\nDir:\t")
 
 dirs = sorted(os.listdir(currentDir))
 print (dirs)
@@ -81,6 +90,7 @@ for filename in dirs:
         filename = filename.replace(")", "")
         filename = filename.replace("[", "")
         filename = filename.replace("]", "")
+        filename = filename.replace("+", "")
         filename = filename.replace("1080p", "")
         filename = filename.replace("BluRay","")
         filename = filename.replace("720p", "")
@@ -98,6 +108,7 @@ for filename in dirs:
         filename = filename.replace("YTS-AM", "")
         filename = filename.replace("."," ")
         years= re.findall('(\d{4})', filename)
+        #moviename = ''
         if len(years) > 0:
             moviename = filename[0:filename.find(years[0])]
             moviename = moviename.replace("."," ")
@@ -108,38 +119,47 @@ for filename in dirs:
             moviename = moviename.rstrip()
             url = 'http://www.omdbapi.com/?apikey={0}&t={1}'.format(omdbapiKey,moviename)
         counter = counter + 1
-        fetchedDetails = requests.get(url)
-        details = fetchedDetails.content
-        json1 = json.loads(details)
-        if json1['Response'] == "False":
-            url = 'https://api.themoviedb.org/3/search/movie?query={0}&api_key={1}'.format(moviename,tmdbKey )
+        #print (moviename)
+        if oldjson.find(moviename)>0:
+            #print (oldjson[oldjson.find(moviename):oldjson.find("]")])
+            jsonpart = '\t\t\t["{0}","{1}]'.format(counter,oldjson[oldjson.find(moviename):oldjson.find("]",oldjson.find(moviename))])
+            finaljson = ''.join([finaljson, jsonpart, " , \n"])
+            #jsonpart = ''
+        else:
             fetchedDetails = requests.get(url)
             details = fetchedDetails.content
-            json2 = json.loads(details)
-            if json2['total_results'] > 0:
-                tmdbID = json2['results'][0]['id']
-                url = 'https://api.themoviedb.org/3/movie/{0}?&api_key={1}'.format(tmdbID,tmdbKey )
+            json1 = json.loads(details)
+            if json1['Response'] == "False":
+                url = 'https://api.themoviedb.org/3/search/movie?query={0}&api_key={1}'.format(moviename,tmdbKey )
                 fetchedDetails = requests.get(url)
                 details = fetchedDetails.content
                 json2 = json.loads(details)
-                imdbID = json2['imdb_id']
-                if imdbID != "":
-                    url = 'http://www.omdbapi.com/?apikey={0}&i={1}'.format(omdbapiKey,imdbID)
+                if json2['total_results'] > 0:
+                    tmdbID = json2['results'][0]['id']
+                    url = 'https://api.themoviedb.org/3/movie/{0}?&api_key={1}'.format(tmdbID,tmdbKey )
                     fetchedDetails = requests.get(url)
                     details = fetchedDetails.content
                     json2 = json.loads(details)
-                    finaljson = dataSetter(json2)
-                else:
+                    imdbID = json2['imdb_id']
+                    if imdbID != "":
+                        url = 'http://www.omdbapi.com/?apikey={0}&i={1}'.format(omdbapiKey,imdbID)
+                        fetchedDetails = requests.get(url)
+                        details = fetchedDetails.content
+                        json2 = json.loads(details)
+                        finaljson = dataSetter(json2)
+                    else:
+                        errorflag = 1
+                        errorfile.write(originalfile + " -- API Error : " + json1['Error'] + " URL : " + url + "\n")
+                else: 
                     errorflag = 1
                     errorfile.write(originalfile + " -- API Error : " + json1['Error'] + " URL : " + url + "\n")
-            else: 
-                errorflag = 1
-                errorfile.write(originalfile + " -- API Error : " + json1['Error'] + " URL : " + url + "\n")
-        else:
-            finaljson = dataSetter(json1)
+            else:
+                finaljson = dataSetter(json1)
 finaljson = finaljson.rstrip(", \n")
 finaljson = ''.join([finaljson, "\n\t\t]\n}"])
 fh = open("content.txt", "w")
 fh.write(finaljson)
 fh.close()
 errorfile.close()
+print("****************")
+closer = input("Done, press Enter: ")
